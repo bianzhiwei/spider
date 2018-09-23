e55bf629519ff5028259190f1a4034202e50a238
bianzhiwei <39261777+bianzhiwei@users.noreply.github.com>
2018-09-23 11:57:58 +0800

    Update config.py

--- reptile_spider/config.py
+++ reptile_spider/config.py
@@ -4,7 +4,6 @@
 config
 Created on: 2018/9/20  16:04
 @author: 卞志伟
-Email:bianzhiwei@iyoujia.com
 """
 import platform
 
@@ -12,8 +11,8 @@ THREAD_SPIDER_NUM = 6  # 爬虫线程
 THREAD_ANALYSIS_NUM = 3  # 解析线程
 
 PROXIES = {
-    'http': 'http://H4D3E584VRA2842D:7CCD05274100107A@http-dyn.abuyun.com:9020',
-    'https': 'http://H4D3E584VRA2842D:7CCD05274100107A@http-dyn.abuyun.com:9020',
+    'http': None,
+    'https': None,
 }
 
 ZHENGUO_DB = 'zhenguo_20180922'
@@ -23,10 +22,10 @@ UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " \
 
 HEADERS = {"User-Agent": UA}
 
-mongodb_url_ = 'mongodb://192.168.80.23:27017/'
+mongodb_url_ = 'mongodb_url_'
 
-mongodb_ip = '192.168.80.23'
-mongodb_port = 27017
+mongodb_ip = 'mongodb_ip'
+mongodb_port = 'mongodb_port'
 
 platform_ = platform.system()
 if platform_ == "Windows":
